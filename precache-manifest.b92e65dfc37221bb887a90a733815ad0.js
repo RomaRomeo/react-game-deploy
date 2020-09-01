2c62597fb36c7db4e34afab5a0e2dadd8c395619
RomaRomeo <r.pidluzhnyi@ukr.net>
2020-09-01 20:26:35 +0300

    Updates

---
+++ precache-manifest.b92e65dfc37221bb887a90a733815ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0cc27ef45bc36b669ac26cce6f462e",
+    "url": "/react-game-deploy/index.html"
+  },
+  {
+    "revision": "d6b9229320026b65d4cb",
+    "url": "/react-game-deploy/static/css/main.6542bd51.chunk.css"
+  },
+  {
+    "revision": "56aa5efe18bd26fbb86c",
+    "url": "/react-game-deploy/static/js/2.ecc28fa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-game-deploy/static/js/2.ecc28fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b9229320026b65d4cb",
+    "url": "/react-game-deploy/static/js/main.9b453cda.chunk.js"
+  },
+  {
+    "revision": "a397e83e2ae2e0746a65",
+    "url": "/react-game-deploy/static/js/runtime-main.846a2f21.js"
+  }
+]);
